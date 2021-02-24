03193a075f39db1845c5840ae8a0cc10f423bb05
benjitour <46075738+benjitour@users.noreply.github.com>
2021-02-24 16:55:43 +0100

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":980},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-open-source-","displayName":"Open Source ","link":{"linkType":"LinkTypePage","href":"#!page-open-source-"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1036},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-open-source-","displayName":"Open Source ","link":{"linkType":"LinkTypePage","href":"#!page-open-source-"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
